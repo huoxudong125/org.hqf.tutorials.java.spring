7184a1dfa9a292057ecdaec5ed4ba40e8c472a50
huoxudong125 <423huo@163.com>
2019-03-18 11:31:49 +0800

    add multi-DataSource using druid

--- aop/src/main/java/org/hqf/tutorials/spring/service/UserServiceImpl.java
+++ aop/src/main/java/org/hqf/tutorials/spring/service/UserServiceImpl.java
@@ -3,16 +3,19 @@ package org.hqf.tutorials.spring.service;
 import org.hqf.tutorials.spring.service.interfaces.UserService;
 
 public class UserServiceImpl implements UserService {
+    @Override
     public void save(long id) {
         System.out.println(String.format("保存用户 %s",id));
 
         //int i = 1/0;
     }
 
+    @Override
     public void delete() {
         System.out.println("删除用户");
     }
 
+    @Override
     public void update() {
         System.out.println("更新用户");
     }
